14b9626280e88303ebe4f702f138ee4afe325608
Ramansaha <ramansaha37@gmail.com>
2025-08-05 09:40:19 +0530

    [feat]: created mongodb connection and added project routes and folder

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import routes from './routes/apiRoutes.js'
+import connectDB from './config/conn.js';
 
 dotenv.config();
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
 
-app.use('/api', (req, res) => {
-    res.json({message : `I am heavy`})
-});
+app.use('/api/ts/', routes);
 
 const PORT = process.env.PORT || 8000
 
